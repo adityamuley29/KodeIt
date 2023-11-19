7d7f43bbf1df1d933bf4c777c2dc11570acf394b
Aditya Muley <adityamuley48@gmail.com>
2023-11-19 21:59:29 +0530

    added deployed url in cors & updated frontend/.env

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const protect = require("./middleware/authMiddleware");
 
 // Middleware
 const corsConfig = {
-  origin: "https://kodeit-backend.vercel.app",
+  origin: ["https://kodeit-backend.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
