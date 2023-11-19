b48b450c043220a57f560df9913be1eac9125692
Aditya Muley <adityamuley48@gmail.com>
2023-11-19 20:51:48 +0530

    updated generateFile.js

--- backend/generateFile.js
+++ backend/generateFile.js
@@ -2,13 +2,13 @@ const path = require("path");
 const fs = require("fs");
 const { v4: uuid } = require("uuid");
 
-const dirCodes = path.join(__dirname, "codes");
-
-if (!fs.existsSync(dirCodes)) {
-  fs.mkdirSync(dirCodes, { recursive: true });
-}
-
 const generateFile = async (format, content) => {
+  const dirCodes = await path.join(process.cwd(), "codes");
+
+  if (!fs.existsSync(dirCodes)) {
+    fs.mkdirSync(dirCodes, { recursive: true });
+  }
+
   const jobId = uuid();
   const fileName = `${jobId}.${format}`;
   const filePath = path.join(dirCodes, fileName);
